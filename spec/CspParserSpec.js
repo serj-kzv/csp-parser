b11234100605fdc6c5ec8f6a620efc03bb3638fb
null <null>
2021-01-22 10:39:02 +0300

    null

--- spec/CspParserSpec.js
+++ spec/CspParserSpec.js
@@ -4,12 +4,16 @@ describe('CspParser',  () => {
     let cspParser;
 
     beforeEach(function() {
-        const csp = "default-src 'self'; script-src 'unsafe-eval' scripts.com scripts-that-will-be-removed.com scripts-that-will-be-removed-2.com scripts-that-will-be-removed-starts-with.com scripts-that-will-be-removed-ends-with.com example-to-be-deleted-by-reg-exp.com; object-src; style-src example.com";
-        cspParser  = new CspParser(csp);
+
     });
 
     it('parse to array', () => {
-        let parsedCsp = cspParser.getPolicy();
+        const csp = "default-src 'self'; script-src 'unsafe-eval' scripts.com scripts-that-will-be-removed.com scripts-that-will-be-removed-2.com scripts-that-will-be-removed-starts-with.com scripts-that-will-be-removed-ends-with.com example-to-be-deleted-by-reg-exp.com; object-src; style-src example.com";
+
+        cspParser  = new CspParser(csp);
+
+        const parsedCsp = cspParser.getPolicy();
+
         expect(parsedCsp[CspDirective.DEFAULT_SRC].length).toEqual(1);
         expect(parsedCsp[CspDirective.SCRIPT_SRC].length).toEqual(7);
         expect(parsedCsp[CspDirective.OBJECT_SRC].length).toEqual(0);
